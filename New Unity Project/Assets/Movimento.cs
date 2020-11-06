b9520f628dce11932bc902040fd0bba78d4a3905
Bianca <b164913@dac.unicamp.br>
2020-11-06 00:35:13 -0300

    Projeto com inputs

---
+++ New Unity Project/Assets/Movimento.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Threading;
+using UnityEngine;
+
+public class Movimento : MonoBehaviour
+{
+    public float velocidade = 10;
+    public Vector3 direcao = new Vector3(-1, 0, 0);
+    public Material Obstáculo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(direcao * velocidade * Time.deltaTime);
+    }
+
+}
---
+++ New Unity Project/Assets/Salto.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Salto : MonoBehaviour
+{
+    public Vector3 direcao = new Vector3Int(0,3,0);
+    public float forca =500;
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            rb.AddForce(direcao * forca);
+        }
+    }
+      private void OnTriggerEnter(Collider other) 
+    {
+        if (other.GetComponent<Obstáculo> () == true)
+        {Morre();
+        }
+    }
+    private void Morre()
+    {
+        Destroy(gameObject);
+    }
+}
